cf38609896f82e78c696ba34de494dd50299a2a5
Mintesinot Necha <165967847+MinteNecha@users.noreply.github.com>
2025-12-26 11:46:15 +0200

    Add comment to describe linear search function

--- linear_search.lua
+++ linear_search.lua
@@ -1,4 +1,6 @@
-function Linear_search(arr, target)
+--Linear search compares target value against all elements in the table
+
+function Linear_search(arr, target) --table/list and target value is required
     for i = 1, #arr do
         if arr[i] == target then
             return print(i.." is in the table.")
