c31eddf1521bb2dd7c55e7540229ba39026c9cb5
Mintesinot Necha <165967847+MinteNecha@users.noreply.github.com>
2025-12-27 01:38:54 +0200

    Add dequeue function to Queue and fix display function for Stack

--- queue.lua
+++ queue.lua
@@ -5,3 +5,13 @@ Queue = {}
 function Queue:enqueue(value)
     table.insert(self, value)
 end    
+
+function Queue:dequeue()
+    table.insert(self, 1)
+end    
+
+function display()
+    for i = 1, #Stack do
+        print(Stack[i])
+    end   
+end    
